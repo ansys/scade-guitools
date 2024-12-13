cb9b99693d5bed8404e6560a307d4c29c34d39d9
Jean Henry <126060567+ansjhenry@users.noreply.github.com>
2024-12-13 11:56:15 +0100

    feat: Initialize the repository (#1)
    * Initialize the repository from the template
    
    * chore: adding changelog file 1.added.md [dependabot-skip]
    
    ---------
    
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

---
+++ doc/source/conf.py
@@ -0,0 +1,130 @@
+"""Sphinx documentation configuration file."""
+
+from datetime import datetime
+import os
+
+from ansys_sphinx_theme import (
+    ansys_favicon,
+    get_version_match,
+)
+
+from ansys.scade.guitools import __version__
+
+# Project information
+project = 'ansys-scade-guitools'
+copyright = f'(c) {datetime.now().year} ANSYS, Inc. All rights reserved'
+author = 'ANSYS, Inc.'
+release = version = __version__
+switcher_version = get_version_match(version)
+
+# Select desired logo, theme, and declare the html title
+html_theme = 'ansys_sphinx_theme'
+html_short_title = html_title = 'Ansys SCADE GUI Tools'
+
+# multi-version documentation
+cname = os.getenv('DOCUMENTATION_CNAME', 'guitools.scade.docs.pyansys.com')
+"""The canonical name of the webpage hosting the documentation."""
+
+# specify the location of your github repo
+html_theme_options = {
+    'github_url': 'https://github.com/ansys/scade-guitools',
+    'show_prev_next': False,
+    'show_breadcrumbs': True,
+    'additional_breadcrumbs': [
+        ('PyAnsys', 'https://docs.pyansys.com/'),
+    ],
+    'switcher': {
+        'json_url': f'https://{cname}/versions.json',
+        'version_match': switcher_version,
+    },
+    'check_switcher': False,
+    'logo': 'pyansys',
+    'ansys_sphinx_theme_autoapi': {
+        'project': project,
+        'own_page_level': 'function',
+        'class_content': 'both',  # documentation in https://sphinxdocs.ansys.com/version/stable/user-guide/autoapi.html
+        'member_order': 'alphabetical',
+        'ignore': ['*/src/ansys/scade/guitools/impl/*', '*/src/ansys/scade/guitools/test/*'],
+    },
+}
+
+# Configuration for Sphinx autoapi
+# TODO autoapi_render_in_single_page = ["class", "enum", "exception", "function"]
+
+# Sphinx extensions
+extensions = [
+    'numpydoc',
+    'sphinx.ext.intersphinx',
+    'sphinx_copybutton',
+    'sphinx_design',
+    'ansys_sphinx_theme.extension.autoapi',
+    'sphinx.ext.autosectionlabel',
+]
+
+# Make sure the target is unique
+autosectionlabel_prefix_document = True
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3.10', None),
+    # kept here as an example
+    # "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
+    # "numpy": ("https://numpy.org/devdocs", None),
+    # "matplotlib": ("https://matplotlib.org/stable", None),
+    # "pandas": ("https://pandas.pydata.org/pandas-docs/stable", None),
+    # "pyvista": ("https://docs.pyvista.org/", None),
+    # "grpc": ("https://grpc.github.io/grpc/python/", None),
+}
+
+# numpydoc configuration
+numpydoc_show_class_members = False
+numpydoc_xref_param_type = True
+
+# Consider enabling numpydoc validation. See:
+# https://numpydoc.readthedocs.io/en/latest/validation.html#
+numpydoc_validate = True
+numpydoc_validation_checks = {
+    'GL06',  # Found unknown section
+    'GL07',  # Sections are in the wrong order.
+    # Disabled the docstring validation as most of the methods doesn't have the docstring
+    # TODO: Add docstring and enable GL08 validation
+    # "GL08",  # The object does not have a docstring
+    'GL09',  # Deprecation warning should precede extended summary
+    'GL10',  # reST directives {directives} must be followed by two colons
+    'SS01',  # No summary found
+    'SS02',  # Summary does not start with a capital letter
+    'SS03',  # Summary does not end with a period
+    'SS04',  # Summary contains heading whitespaces
+    # "SS05", # Summary must start with infinitive verb, not third person
+    'RT02',  # The first line of the Returns section should contain only the
+    # type, unless multiple values are being returned"
+}
+
+# Favicon
+html_favicon = ansys_favicon
+
+# static path
+html_static_path = ['_static']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# TODO: remove ignore links after public release
+linkcheck_ignore = [
+    'https://github.com/ansys/scade-guitools',
+    'https://github.com/ansys/scade-guitools/actions/workflows/ci_cd.yml',
+    'https://pypi.org/project/ansys-scade-guitools',
+    # The link below takes a long time to check
+    'https://www.ansys.com/products/embedded-software/ansys-scade-suite',
+    'https://www.ansys.com/*',
+]
+
+
+if switcher_version != 'dev':
+    linkcheck_ignore.append(f'https://github.com/ansys/scade-guitools/releases/tag/v{__version__}')
